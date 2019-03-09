7d617e514a38fba137afce3c9f1a8533bd9ed555
ltl0814 <42437316+ltl0814@users.noreply.github.com>
2019-03-09 18:34:12 +0800

    Update test.java
    chang 111 to 11

--- src/main/java/com/synnex/superonlinestore/util/test.java
+++ src/main/java/com/synnex/superonlinestore/util/test.java
@@ -3,6 +3,6 @@ package com.synnex.superonlinestore.util;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("111");
+        System.out.println("11");
     }
 }
