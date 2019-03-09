6f2f10cae8f9bae7d0372d738fa55371807144da
ltl <1163227244@qq.com>
2019-03-09 18:58:32 +0800

    change 11 to bb

--- src/main/java/com/synnex/superonlinestore/util/test.java
+++ src/main/java/com/synnex/superonlinestore/util/test.java
@@ -3,6 +3,6 @@ package com.synnex.superonlinestore.util;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("11");
+        System.out.println("bb");
     }
 }
